235f48f81b8be85c2d7b6dd6a6fe90e8dca06525
John A. Stevens <john@otherskylines.com>
2020-07-25 11:23:32 -0600

    GUI: Vertically center highlights on highlighted text.

--- src/Linediff/GuiDiffFormatter.cs
+++ src/Linediff/GuiDiffFormatter.cs
@@ -49,7 +49,7 @@ namespace Linediff
 
         private float Render(ChangedTextBlock block)
         {
-            var rectPos = ImGui.GetCursorPos() + ImGui.GetWindowPos() - new Vector2(0, lastScrollY);// - ImGui.GetStyle().ItemSpacing / 2.0f;
+            var rectPos = ImGui.GetCursorPos() + ImGui.GetWindowPos() - new Vector2(0, lastScrollY) - new Vector2(0,ImGui.GetStyle().ItemSpacing.Y/2.0f);
             var textWidth = ImGui.CalcTextSize(block.Text).X;
 
             ImGui.GetWindowDrawList().AddRectFilled(rectPos, rectPos + new Vector2(textWidth, lineHeight), (uint)Color.Magenta.ToArgb());
@@ -61,7 +61,7 @@ namespace Linediff
 
         private float Render(AddedTextBlock block)
         {
-            var rectPos = ImGui.GetCursorPos() + ImGui.GetWindowPos() - new Vector2(0, lastScrollY);// - ImGui.GetStyle().ItemSpacing / 2.0f;
+            var rectPos = ImGui.GetCursorPos() + ImGui.GetWindowPos() - new Vector2(0, lastScrollY) - new Vector2(0, ImGui.GetStyle().ItemSpacing.Y / 2.0f);
             var textWidth = ImGui.CalcTextSize(block.Text).X;
 
             ImGui.GetWindowDrawList().AddRectFilled(rectPos, rectPos + new Vector2(textWidth, lineHeight), (uint)Color.Green.ToArgb());
@@ -73,7 +73,7 @@ namespace Linediff
 
         private float Render(RemovedTextBlock block)
         {
-            var rectPos = ImGui.GetCursorPos() + ImGui.GetWindowPos() - new Vector2(0, lastScrollY);// - ImGui.GetStyle().ItemSpacing / 2.0f;
+            var rectPos = ImGui.GetCursorPos() + ImGui.GetWindowPos() - new Vector2(0, lastScrollY) - new Vector2(0, ImGui.GetStyle().ItemSpacing.Y / 2.0f);
             var textWidth = ImGui.CalcTextSize(block.Text).X;
 
             ImGui.GetWindowDrawList().AddRectFilled(rectPos, rectPos + new Vector2(textWidth, lineHeight), (uint)Color.Red.ToArgb());
